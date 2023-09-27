d79c5d1e43a2502e2d5e6477a11ce1dd1a427982
Andronov Ivan (Ber Tea) <andronoviv4@gmail.com>
2023-09-27 22:53:12 +0300

    commit for my HomeWork

--- src/Lives.java
+++ src/Lives.java
@@ -1,27 +1,35 @@
-public class Lives extends Creep{
+public class Lives extends Creep {
 
     public Lives(int jumpMaxHeight, int runMaxDistance, String name) {
         super(jumpMaxHeight, runMaxDistance, name);
     }
-
+    /**
+     * Метод оценивает осилит ли прыжок
+     * @param jumpHeight передаем выссоту препятсвия
+     * @return true/false
+     */
     @Override
     boolean jumping(int jumpHeight) {
-        if (jumpMaxHeight<jumpHeight){
-            System.out.println(name+"непотянул прыжок, убит");
+        if (jumpMaxHeight < jumpHeight) {
+            System.out.println(this.getClass().getSimpleName() + " "  + name + " Непотянул прыжок, и выбывает из соревнования");
             return false;
-        }else {
-            System.out.println(name + "Прыгнул" + jumpHeight);
+        } else {
+            System.out.println(this.getClass().getSimpleName() + " "  + name + " Прыгнул " + jumpHeight + " высоту стены");
             return true;
         }
-        }
-
+    }
+    /**
+     * Метод оценивает осилит ли беговую дорожку
+     * @param runDistance передаем длинну препятсвия
+     * @return true/false
+     */
     @Override
     boolean running(int runDistance) {
-        if(runMaxDistance<runDistance){
-            System.out.println(name+"Непотянул беговую дорожку, убит");
+        if (runMaxDistance < runDistance) {
+            System.out.println(this.getClass().getSimpleName() + " "  + name + " Непотянул беговую дорожку, и выбывает из соревнования");
             return false;
-        }else {
-            System.out.println(name + "Пробежал" + runDistance);
+        } else {
+            System.out.println(this.getClass().getSimpleName() + " " + name + " Пробежал " + runDistance + " по беговой дорожке");
             return true;
         }
     }
